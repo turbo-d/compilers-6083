c0082b9842e85bab8b7d7e14df038cf3ab9efcd8
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2024-01-11 12:38:15 -0500

    Add command line input and load file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    println!("Compiling {}", file_path);
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("{}", contents)
 }
