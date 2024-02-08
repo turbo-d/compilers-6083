f45d11ae1dfa03708fa65bb2fc56602480f438c8
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2024-02-08 16:52:58 -0500

    Add common case scanner tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
 #[derive(PartialEq)]
 pub enum Token {
     EOF,
+    Bool,
+    Type,
+    Program,
+    Is,
+    Global,
+    Procedure,
+    Variable,
+    Begin,
+    EndProgram,
+    EndProcedure,
+    EndIf,
+    EndFor,
+    If,
+    Then,
+    Else,
+    For,
+    Return,
     Identifier,
     Number,
     String,
@@ -15,8 +32,10 @@ pub enum Token {
     Plus,
     Minus,
     Multiply,
+    Divide,
     And,
     Or,
+    Not,
     Assign,
     LT,
     LTE,
@@ -303,4 +322,435 @@ impl Scanner {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn scan_add() {
+        let mut s = Scanner {
+            stream: String::from("+"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Plus);
+    }
+
+    #[test]
+    fn scan_sub() {
+        let mut s = Scanner {
+            stream: String::from("-"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Minus);
+    }
+
+    #[test]
+    fn scan_mul() {
+        let mut s = Scanner {
+            stream: String::from("*"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Multiply);
+    }
+
+    #[test]
+    fn scan_div() {
+        let mut s = Scanner {
+            stream: String::from("/"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Divide);
+    }
+
+    #[test]
+    fn scan_lt() {
+        let mut s = Scanner {
+            stream: String::from("<"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::LT);
+    }
+
+    #[test]
+    fn scan_lte() {
+        let mut s = Scanner {
+            stream: String::from("<="),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::LTE);
+    }
+
+    #[test]
+    fn scan_gt() {
+        let mut s = Scanner {
+            stream: String::from(">"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::GT);
+    }
+
+    #[test]
+    fn scan_gte() {
+        let mut s = Scanner {
+            stream: String::from(">="),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::GTE);
+    }
+
+    #[test]
+    fn scan_equal() {
+        let mut s = Scanner {
+            stream: String::from("=="),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Equal);
+    }
+
+    #[test]
+    fn scan_notequal() {
+        let mut s = Scanner {
+            stream: String::from("!="),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::NotEqual);
+    }
+
+    #[test]
+    fn scan_and() {
+        let mut s = Scanner {
+            stream: String::from("&"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::And);
+    }
+
+    #[test]
+    fn scan_or() {
+        let mut s = Scanner {
+            stream: String::from("|"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Or);
+    }
+
+    #[test]
+    fn scan_not() {
+        let mut s = Scanner {
+            stream: String::from("not"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Not);
+    }
+
+    #[test]
+    fn scan_bool_true() {
+        let mut s = Scanner {
+            stream: String::from("true"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Bool);
+    }
+
+    #[test]
+    fn scan_bool_false() {
+        let mut s = Scanner {
+            stream: String::from("false"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Bool);
+    }
+
+    #[test]
+    fn scan_type_integer() {
+        let mut s = Scanner {
+            stream: String::from("integer"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Type);
+    }
+
+    #[test]
+    fn scan_type_float() {
+        let mut s = Scanner {
+            stream: String::from("float"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Type);
+    }
+
+    #[test]
+    fn scan_type_string() {
+        let mut s = Scanner {
+            stream: String::from("string"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Type);
+    }
+
+    #[test]
+    fn scan_type_bool() {
+        let mut s = Scanner {
+            stream: String::from("bool"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Type);
+    }
+
+    #[test]
+    fn scan_program() {
+        let mut s = Scanner {
+            stream: String::from("program"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Program);
+    }
+
+    #[test]
+    fn scan_is() {
+        let mut s = Scanner {
+            stream: String::from("is"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Is);
+    }
+
+    #[test]
+    fn scan_global() {
+        let mut s = Scanner {
+            stream: String::from("global"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Global);
+    }
+
+    #[test]
+    fn scan_procedure() {
+        let mut s = Scanner {
+            stream: String::from("procedure"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Procedure);
+    }
+
+    #[test]
+    fn scan_variable() {
+        let mut s = Scanner {
+            stream: String::from("variable"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Variable);
+    }
+
+    #[test]
+    fn scan_begin() {
+        let mut s = Scanner {
+            stream: String::from("begin"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Begin);
+    }
+
+    #[test]
+    fn scan_endprogram() {
+        let mut s = Scanner {
+            stream: String::from("end program"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::EndProgram);
+    }
+
+    #[test]
+    fn scan_endprocedure() {
+        let mut s = Scanner {
+            stream: String::from("end procedure"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::EndProcedure);
+    }
+
+    #[test]
+    fn scan_endif() {
+        let mut s = Scanner {
+            stream: String::from("end if"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::EndIf);
+    }
+
+    #[test]
+    fn scan_endfor() {
+        let mut s = Scanner {
+            stream: String::from("end for"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::EndFor);
+    }
+
+    #[test]
+    fn scan_if() {
+        let mut s = Scanner {
+            stream: String::from("if"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::If);
+    }
+
+    #[test]
+    fn scan_then() {
+        let mut s = Scanner {
+            stream: String::from("then"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Then);
+    }
+
+    #[test]
+    fn scan_else() {
+        let mut s = Scanner {
+            stream: String::from("else"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Else);
+    }
+
+    #[test]
+    fn scan_for() {
+        let mut s = Scanner {
+            stream: String::from("for"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::For);
+    }
+
+    #[test]
+    fn scan_return() {
+        let mut s = Scanner {
+            stream: String::from("return"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Return);
+    }
+
+    #[test]
+    fn scan_period() {
+        let mut s = Scanner {
+            stream: String::from("."),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Period);
+    }
+
+    #[test]
+    fn scan_semicolon() {
+        let mut s = Scanner {
+            stream: String::from(";"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Semicolon);
+    }
+
+    #[test]
+    fn scan_colon() {
+        let mut s = Scanner {
+            stream: String::from(":"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Colon);
+    }
+
+    #[test]
+    fn scan_leftparen() {
+        let mut s = Scanner {
+            stream: String::from("("),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::LeftParen);
+    }
+
+    #[test]
+    fn scan_rightparen() {
+        let mut s = Scanner {
+            stream: String::from(")"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::RightParen);
+    }
+
+    #[test]
+    fn scan_leftsquare() {
+        let mut s = Scanner {
+            stream: String::from("["),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::LeftSquare);
+    }
+
+    #[test]
+    fn scan_rightsquare() {
+        let mut s = Scanner {
+            stream: String::from("]"),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::RightSquare);
+    }
+
+    #[test]
+    fn scan_assignment() {
+        let mut s = Scanner {
+            stream: String::from(":="),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Assign);
+    }
+
+    #[test]
+    fn scan_comma() {
+        let mut s = Scanner {
+            stream: String::from(","),
+            i: 0,
+        };
+        let tok = s.scan();
+        assert!(tok == Token::Comma);
+    }
 }
