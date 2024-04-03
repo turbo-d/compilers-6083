4cc2e4a431d5a358a353ed5dd0140b87f1e99f21
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2024-04-03 17:53:13 -0400

    Move dummy runtime procedures to LLParser constructor

--- src/llparser.rs
+++ src/llparser.rs
@@ -13,25 +13,27 @@ pub struct LLParser {
 
 impl LLParser {
     pub fn new(s: Scanner) -> LLParser {
+        let mut st = SymTable::new();
+        // TODO: Delete this once runtime is finished.
+        // This is just for testing
+        let _ = st.insert_global(String::from("getbool"), Types::Proc(Box::new(Types::Bool), Vec::new()));
+        let _ = st.insert_global(String::from("getinteger"), Types::Proc(Box::new(Types::Int), Vec::new()));
+        let _ = st.insert_global(String::from("getfloat"), Types::Proc(Box::new(Types::Float), Vec::new()));
+        let _ = st.insert_global(String::from("getstring"), Types::Proc(Box::new(Types::String), Vec::new()));
+        let _ = st.insert_global(String::from("putbool"), Types::Proc(Box::new(Types::Bool), vec![Types::Bool]));
+        let _ = st.insert_global(String::from("putinteger"), Types::Proc(Box::new(Types::Bool), vec![Types::Int]));
+        let _ = st.insert_global(String::from("putfloat"), Types::Proc(Box::new(Types::Bool), vec![Types::Float]));
+        let _ = st.insert_global(String::from("putstring"), Types::Proc(Box::new(Types::Bool), vec![Types::String]));
+        let _ = st.insert_global(String::from("sqrt"), Types::Proc(Box::new(Types::Float), vec![Types::Int]));
+
         LLParser {
             s,
             tok: Token::Unknown,
-            st: SymTable::new(),
+            st: st,
         }
     }
 
     pub fn parse(&mut self) {
-        // TODO: Delete this once runtime is finished.
-        // This is just for testing
-        let _ = self.st.insert_global(String::from("getbool"), Types::Proc(Box::new(Types::Bool), Vec::new()));
-        let _ = self.st.insert_global(String::from("getinteger"), Types::Proc(Box::new(Types::Int), Vec::new()));
-        let _ = self.st.insert_global(String::from("getfloat"), Types::Proc(Box::new(Types::Float), Vec::new()));
-        let _ = self.st.insert_global(String::from("getstring"), Types::Proc(Box::new(Types::String), Vec::new()));
-        let _ = self.st.insert_global(String::from("putbool"), Types::Proc(Box::new(Types::Bool), vec![Types::Bool]));
-        let _ = self.st.insert_global(String::from("putinteger"), Types::Proc(Box::new(Types::Bool), vec![Types::Int]));
-        let _ = self.st.insert_global(String::from("putfloat"), Types::Proc(Box::new(Types::Bool), vec![Types::Float]));
-        let _ = self.st.insert_global(String::from("putstring"), Types::Proc(Box::new(Types::Bool), vec![Types::String]));
-        let _ = self.st.insert_global(String::from("sqrt"), Types::Proc(Box::new(Types::Float), vec![Types::Int]));
 
         self.consume_tok();
 
