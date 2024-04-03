3b18802b6c91aee1e5605a5fca4d94bb7e9eed0c
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2024-04-03 13:13:36 -0400

    Add debug printing to Types enum

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::vec::Vec;
 
 #[derive(Clone, PartialEq)]
@@ -10,4 +11,26 @@ pub enum Types {
     Bool,
     Proc(Box<Types>, Vec<Types>),
     Array(u32, Box<Types>),
+}
+
+impl fmt::Display for Types {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Types::Unknown => write!(f, "<Types::Unknown>"),
+            Types::None => write!(f, "<Types::None>"),
+            Types::Int => write!(f, "<Types::Int>"),
+            Types::Float => write!(f, "<Types::Float>"),
+            Types::String => write!(f, "<Types::String>"),
+            Types::Bool => write!(f, "<Types::Bool>"),
+            Types::Proc(return_type, param_types) => {
+                let _ = write!(f, "<Types::Proc(");
+                let _ = write!(f, "{}, {{", *return_type);
+                for param_type in param_types.iter() {
+                    let _ = write!(f, "{}, ", param_type);
+                }
+                write!(f, "}})>")
+            }
+            Types::Array(size, base_type) => write!(f, "<Types::Array({}, {})>", size, *base_type),
+       }
+    }
 }
